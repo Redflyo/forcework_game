784a0aee9837e3e5efae6743a124475db4a81ae8
Remi Epagneaud <remi.epagneaud@etudiant.univ-lr.fr>
2021-06-05 23:33:22 +0200

    ajout main.cpp

--- main.cpp
+++ main.cpp
@@ -7,16 +7,6 @@
 
 int main(int argc, char *argv[])
 {
-  /*int pos;
-  double score0=31.2, score1=6.2, score2=21.6, score3=78, score4=12.8, score5=98.6;
-  std::string name0="remi", name1="enzo", name2="rmi", name3="nzo", name4="rem", name5="enz";
-  Settings setting("../forceWork/lemurdeschampions.txt");
-  setting.writeTempleFile(name0, score0, pos);
-  qDebug() << "yo";
-  if(setting.isTopFive(score1, pos)) qDebug()<<"jesuismeilleur";setting.writeTempleFile(name1, score1, pos);
-*/
-
-
 
   QApplication a(argc, argv);
   MainWindow w;
