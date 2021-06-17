8c58bedc53e02c515c7b3d3c50909ef6d3332b58
Hugo Magnan <hugo.magnan@etudiant.univ-lr.fr>
2021-06-17 09:56:42 +0200

    backup

--- forcework.cpp
+++ forcework.cpp
@@ -189,7 +189,7 @@ ForceWork::ForceWork(Settings * settings)
     itsSettings = settings;
     Map map;
     itsMap = map;
-    itsPersonnages = itsMap.loadMap("../forcework_game/data/mapIntro.txt");
+    itsPersonnages = itsMap.loadMap("../forcework_game/data/mapTest.txt");
     camera = new Camera(itsMap.getItsWidthMap(),itsMap.getItsHeightMap());
 
 
